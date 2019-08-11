883275fbd44e8d14e6f9166adca380b7a56d363f
Diego Turco <diego.turco@tecsinapse.com.br>
2019-08-11 17:55:11 -0300

    Added componentDidMount and componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   async function fetchData() {
     const response = await fetch('https://api.github.com/users/diegoturco/repos');
     const data = await response.json();
+    
     setRepositories(data);
   }
 
@@ -13,10 +14,28 @@ export default function App() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `You have ${filtered.length} favorites`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo      
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span> (Favorite) </span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favorite</button>
+        </li>
       ))}
     </ul>
   );
